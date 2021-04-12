367303d7f0830460591f02d94bf3dc2cb15234e9
Aboish <louis.bouthillon@student-cs.fr>
2021-04-12 23:22:23 +0200

    ajout_du_while_pour_query

--- api/api.py
+++ api/api.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     lat = 48.89525193
     lon = 2.247122897
 
+    #init du radius
+    radius = 1
     # wrap the query
 
     # query
@@ -13,13 +15,40 @@ if __name__ == "__main__":
     r = api.query(
         """
     <query type="way">
-    <around lat="48.89525193" lon="2.247122897" radius="5"/>
+    <around lat="{}" lon="{}" radius="{}"/>
     </query>
     <print />
-    """
+    """.format(lat,lon,radius)
     )
+
+    nb_way=len(r.ways)
+    while nb_way!=1:
+        if nb_way==0:
+            radius = 2*radius
+            r = api.query(
+            """
+            <query type="way">
+            <around lat="{}" lon="{}" radius="{}"/>
+            </query>
+            <print />
+            """.format(lat,lon,radius)
+            )
+        else:
+            radius = 3/4*radius
+            r = api.query(
+            """
+            <query type="way">
+            <around lat="{}" lon="{}" radius="{}"/>
+            </query>
+            <print />
+            """.format(lat,lon,radius)
+            )
+        nb_way=len(r.ways)
 
     # query output processing
     way = r.ways[0]
-    print(way.tags["name"])
+    print(way.get_nodes(resolve_missing=True))
     # print(r.ways)
+
+    # way.tags["name"],
+
