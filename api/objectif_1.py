adfb44b6dcd31f7d817b8b6a0fae6322d0996b36
Lembe Vardamides Emmanuel <lembe.vardamides@student-cs.fr>
2021-04-14 22:05:20 +0200

    objectif 2

--- api/objectif_1.py
+++ api/objectif_1.py
@@ -1,24 +1,15 @@
 import overpy
 from geopy.geocoders import Nominatim
 
-if __name__ == "__main__":
-    """
-    TO DO
-    - debug all the locations
-    - other objectives
-    - get lat and lon as parameters
-    """
- 
+# get coords
+lat = 48.89227652
+lon = 2.253773690
 
-    # get coords
-    lat =   48.89227652 
-    lon =   2.253773690 
+api = overpy.Overpass()
 
-    # lat =   48.89394122 
-    # lon =   2.247959188 
-    
-    # lat = 48.89525193
-    # lon = 2.247122897
+
+def find_city_way(lat, lon):
+
     # retrieve city and way
     locator = Nominatim(user_agent="myGeocoder")
     coordinates = "{}, {}".format(lat, lon)
@@ -30,7 +21,7 @@ if __name__ == "__main__":
     radius = 1
 
     # query
-    api = overpy.Overpass()
+
     r = api.query(
         """
     <query type="way">
@@ -58,14 +49,14 @@ if __name__ == "__main__":
                 )
             )
         else:
-            nb_route_nommees=0
-            indice=0
-            while nb_route_nommees<2 and indice<nb_way:
+            nb_route_nommees = 0
+            indice = 0
+            while nb_route_nommees < 2 and indice < nb_way:
                 if 'name' in r.ways[indice].tags.keys():
-                    nb_route_nommees+=1
-                    way=r.ways[indice]
-                indice+=1
-            if nb_route_nommees==2:
+                    nb_route_nommees += 1
+                    way = r.ways[indice]
+                indice += 1
+            if nb_route_nommees == 2:
                 radius = 3 / 4 * radius
                 r = api.query(
                     """
@@ -77,7 +68,7 @@ if __name__ == "__main__":
                         lat, lon, radius
                     )
                 )
-            elif nb_route_nommees==0:
+            elif nb_route_nommees == 0:
                 radius = 2 * radius
                 r = api.query(
                     """
@@ -90,26 +81,24 @@ if __name__ == "__main__":
                     )
                 )
             else:
-                route_trouvee=True
+                route_trouvee = True
 
-            
         nb_way = len(r.ways)
 
-    def prod_scalaire(vect1, vect2):
-        lat1, lon1 = vect1
-        lat2, lon2 = vect2
-        return lat1 * lat2 + lon1 * lon2
+    return way, addr
 
-    def norme_carre(vect):
-        return prod_scalaire(vect, vect)
 
-    # query output processing
-    # way = r.ways[0]
-    nom = way.tags["name"]
-    # ville = way.tags["addr:city"]
-    nodes = way.get_nodes(resolve_missing=True)
-    # print(list(map(lambda node: node.id,nodes)))
-    # verification que way a bien un seul element
+def prod_scalaire(vect1, vect2):
+    lat1, lon1 = vect1
+    lat2, lon2 = vect2
+    return lat1 * lat2 + lon1 * lon2
+
+
+def norme_carre(vect):
+    return prod_scalaire(vect, vect)
+
+
+def find_nearest_section(nodes):
 
     # trouver le segment le plus proche de l'arbre
     distance_min_carre = 10000
@@ -130,43 +119,58 @@ if __name__ == "__main__":
             distance_min_carre = distance
             indice_min = indice
 
-    def trouver_intersection(nodes, indice, direction, nom_route):
-        lat = float(nodes[indice].lat)
-        lon = float(nodes[indice].lon)
-        result = api.query(
-            """
-            <query type="way">
-            <around lat="{}" lon="{}" radius="0"/>
-            </query>
-            <print />
-            """.format(
-                lat, lon
-            )
-        )
-        ways = result.ways
-        if len(ways) > 1:
-            for i in range(len(ways)):
-                if "name" in ways[i].tags.keys() and ways[i].tags["name"] != nom_route:
-                    return ways[i].tags["name"]
-        if indice + direction < 0:
-            return "debut de route"
-        elif indice + direction >= len(nodes):
-            return "fin de route"
-        else:
-            return trouver_intersection(nodes, indice + direction, direction, nom_route)
+    return indice_min
 
+
+def trouver_intersection(nodes, indice, direction, nom_route):
+
+    lat = float(nodes[indice].lat)
+    lon = float(nodes[indice].lon)
+    result = api.query(
+        """
+        <query type="way">
+        <around lat="{}" lon="{}" radius="0"/>
+        </query>
+        <print />
+        """.format(
+            lat, lon
+        )
+    )
+    ways = result.ways
+    if len(ways) > 1:
+        for i in range(len(ways)):
+            if "name" in ways[i].tags.keys() and ways[i].tags["name"] != nom_route:
+                return ways[i].tags["name"]
+    if indice + direction < 0:
+        return "debut de route"
+    elif indice + direction >= len(nodes):
+        return "fin de route"
+    else:
+        return trouver_intersection(nodes, indice + direction, direction, nom_route)
+
+
+def give_location(nodes, name, adrr):
+    indice_min = find_nearest_section(nodes)
+    print('indice_min:', indice_min)
     intersection1, intersection2 = (
-        trouver_intersection(nodes, indice_min, -1, nom),
-        trouver_intersection(nodes, indice_min + 1, 1, nom),
+        trouver_intersection(nodes, indice_min, -1, name),
+        trouver_intersection(nodes, indice_min + 1, 1, name),
     )
     # print(addr)
+    print('intersection 1:', intersection1,
+          'intersection 2:', intersection2)
     print(
-        "Sur la "
-        + nom
-        + " entre la "
+        "Sur "
+        + name
+        + " entre "
         + intersection1
-        + " et la "
+        + " et "
         + intersection2
         + " dans la ville de "
         + addr[-7]
     )
+
+
+way, addr = find_city_way(lat, lon)
+print('way:', way, 'addr:', addr)
+give_location(way.get_nodes(resolve_missing=True), way.tags['name'], addr)
