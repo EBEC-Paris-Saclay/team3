591b32832f617d34db5eb984a16ad217f36c7127
Aboish <louis.bouthillon@student-cs.fr>
2021-04-15 09:38:46 +0200

    comments_obj_3

--- api/objectif_3.py
+++ api/objectif_3.py
@@ -17,14 +17,9 @@ if __name__ == "__main__":
     lat_arbre_2 =   48.89627806 
     lon_arbre_2 =   2.248657510
 
-    # lat =   48.89394122 
-    # lon =   2.247959188 
-    
-    # lat = 48.89525193
-    # lon = 2.247122897
-    # retrieve city and way
+    # retrieve city and way 
     locator = Nominatim(user_agent="myGeocoder")
-    coordinates = "{}, {}".format(lat_arbre_1, lon_arbre_1)
+    coordinates = "{}, {}".format(lat_arbre_1, lon_arbre_1) #the two trees need to be on the same way so we juste need to find the way of the first one
     location = locator.reverse(coordinates)
     addr = location.address
     addr = addr.split(",")
@@ -34,6 +29,7 @@ if __name__ == "__main__":
 
     # query
     api = overpy.Overpass()
+    #first query with a thin radius
     r = api.query(
         """
     <query type="way">
@@ -47,7 +43,10 @@ if __name__ == "__main__":
 
     nb_way = len(r.ways)
     route_trouvee = False
+    #the call of the query is repeated with different values of radius in order to find a query result with only one named way 
+    #The found way is necessarily the nearest named way of the tree
     while not(route_trouvee):
+        #if there is no way in the result of the previuos query, we increase the radius by a factor 3/2
         if nb_way == 0:
             radius = 3/2 * radius
             r = api.query(
@@ -63,11 +62,13 @@ if __name__ == "__main__":
         else:
             nb_route_nommees=0
             indice=0
+            #if there are ways in the result of the previous query result, we want to know if there is 0, 1 or 2+ named ways in the result
             while nb_route_nommees<2 and indice<nb_way:
                 if 'name' in r.ways[indice].tags.keys():
                     nb_route_nommees+=1
                     way=r.ways[indice]
                 indice+=1
+            #if there is at least two ways, we reduce the radius by a factor 5/6
             if nb_route_nommees==2:
                 radius = 5 / 6 * radius
                 r = api.query(
@@ -80,6 +81,7 @@ if __name__ == "__main__":
                         lat_arbre_1, lon_arbre_1, radius
                     )
                 )
+            #if there is no named way in the result, we increase the radius by a factor 3/2
             elif nb_route_nommees==0:
                 radius = 3/2 * radius
                 r = api.query(
@@ -92,37 +94,41 @@ if __name__ == "__main__":
                         lat_arbre_1, lon, radius
                     )
                 )
+            #if there is only one named way in the result, we end the while.
             else:
                 route_trouvee=True
 
             
         nb_way = len(r.ways)
+#Now that we have the way , we want to find the segments between to nodes that contains the the trees
+#Beware that in openstreemap a node is not necessarly a intersection between ways but it can be just to fit the curve of the way
+#We consider that, in the scale of a city, the couple of coordinates (lat,lon) defines a Cartesian coordinate system 
 
+    #prod_scalaire returns the scalar product of two vectors. 
     def prod_scalaire(vect1, vect2):
         lat1, lon1 = vect1
         lat2, lon2 = vect2
         return lat1 * lat2 + lon1 * lon2
 
+    
+    #norme_carre returns the square of the norme of e vector
     def norme_carre(vect):
         return prod_scalaire(vect, vect)
 
-    # query output processing
-    # way = r.ways[0]
+    #get name of the way and the nodes it contains
     nom = way.tags["name"]
-    # ville = way.tags["addr:city"]
     nodes = way.get_nodes(resolve_missing=True)
-    # print(list(map(lambda node: node.id,nodes)))
-    # verification que way a bien un seul element
 
-    # trouver le segment le plus proche de l'arbre
+    # now we want to find the closest segment and the index of its first node in the list nodes
     def trouver_indice_min(lat,lon):
-        distance_min_carre = 10000
+        distance_min_carre = 10000 #we initiate the minimum distance at a large value of 10 kilometers
         indice_min = 0
         for indice in range(len(nodes) - 1):
             lat1 = float(nodes[indice].lat)
             lon1 = float(nodes[indice].lon)
             lat2 = float(nodes[indice + 1].lat)
             lon2 = float(nodes[indice + 1].lon)
+            #now we calculate the distance between one tree and a segment
             t = prod_scalaire(
                 (lat - lat1, lon - lon1), (lat2 - lat1, lon2 - lon1)
             ) / norme_carre((lat2 - lat1, lon2 - lon1))
@@ -130,17 +136,21 @@ if __name__ == "__main__":
             latH = lat1 + (lat2 - lat1) * t
             lonH = lon1 + (lon2 - lon1) * t
             distance = norme_carre((lat - latH, lon - lonH))
+            #So we update indice_min which is the index of the node at the begining of the closest segment
             if distance_min_carre > distance:
                 distance_min_carre = distance
                 indice_min = indice
         return indice_min
     
+    #we use trouver_indice_min to find the begining node of each segment
     indice_min_1 = trouver_indice_min(lat_arbre_1,lon_arbre_1)
     indice_min_2 = trouver_indice_min(lat_arbre_2,lon_arbre_2)
 
+    #trouver intersection returns the first intersection at the right (if direction values 1) or left (if it values -1) of a node represented by its index in nodes
     def trouver_intersection(nodes, indice, direction, nom_route):
         lat_node = float(nodes[indice].lat)
         lon_node = float(nodes[indice].lon)
+        #this query returns the list of ways the node is in.
         result = api.query(
             """
             <query type="way">
@@ -152,10 +162,12 @@ if __name__ == "__main__":
             )
         )
         ways = result.ways
+        #we want to find a node that is an intersection between at least two NAMED ways
         if len(ways) > 1:
             for i in range(len(ways)):
                 if "name" in ways[i].tags.keys() and ways[i].tags["name"] != nom_route:
                     return ways[i].tags["name"]
+        #It is possible that the tree is at the very last segment or the very first segment of the road and there is not necessarly another way at the end and at the beginning of a way
         if indice + direction < 0:
             return "debut de route"
         elif indice + direction >= len(nodes):
@@ -163,6 +175,7 @@ if __name__ == "__main__":
         else:
             return trouver_intersection(nodes, indice + direction, direction, nom_route)
 
+    #depending on the relative position of the two trees in the way, we find the two intersections we need
     if indice_min_1<indice_min_2:
         intersection1=trouver_intersection(nodes, indice_min_1, -1, nom)
         intersection2=trouver_intersection(nodes, indice_min_2+1, 1, nom)
@@ -172,13 +185,13 @@ if __name__ == "__main__":
 
     
 
-    # print(addr)
+    #the algorithm print the result in natural language
     print(
-        "Sur la "
+        "Sur  "
         + nom
-        + " entre la "
+        + " entre  "
         + intersection1
-        + " et la "
+        + " et  "
         + intersection2
         + " dans la ville de"
         + addr[-7]
